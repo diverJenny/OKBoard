ac8c2ec7984bae23b4d1a3480791d095b1645265
Jenny <jenny@Jennys-MacBook-Pro.local>
2023-07-12 22:06:06 +0900

    Initial commit

---
+++ src/main/java/com/example/okboard/OkBoardApplication.java
@@ -0,0 +1,13 @@
+package com.example.okboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OkBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OkBoardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/okboard/OkBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.okboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OkBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
