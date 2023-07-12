2d60aa623b4a748caba3c35dbf157a012a4025d7
Jenny <jenny@Jennys-MacBook-Pro.local>
2023-07-13 00:53:29 +0900

    fix: 로그인 기능 에러 해결중

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,11 +1,22 @@
-<script setup>
-
-</script>
-
 <template>
+  <v-layout class="rounded rounded-md">
+    <v-app-bar color="primary" density="compact">
+      <v-app-bar-title>Photos</v-app-bar-title>
+    </v-app-bar>
 
+    <v-navigation-drawer>
+      <v-list>
+        <v-list-item title="Navigation drawer"></v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-main
+      class="d-flex align-center justify-center"
+      style="min-height: 300px"
+    >
+      Main Content
+    </v-main>
+  </v-layout>
 </template>
-
-<style scoped>
-
-</style>
+<script setup></script>
+<style scoped></style>
--- frontend/src/views/SignIn.vue
+++ frontend/src/views/SignIn.vue
@@ -1,11 +1,48 @@
-<script setup>
-
-</script>
-
+<!-- SignIn.vue -->
 <template>
+  <v-sheet width="300" class="mx-auto">
+    <v-form @submit.prevent="submit">
+      <v-text-field v-model="state.form.email" label="e-mail"></v-text-field>
+      <v-text-field
+        v-model="state.form.password"
+        label="password"
+      ></v-text-field>
 
+      <v-btn block class="mt-2" @click="submit">Login</v-btn>
+    </v-form>
+  </v-sheet>
 </template>
 
-<style scoped>
+<!--<script setup>-->
+<script setup>
+import { reactive } from "vue";
+import axios from "axios";
+import router from "@/router";
 
-</style>
+const state = reactive({
+  form: {
+    email: "",
+    password: "",
+  },
+});
+
+const submit = () => {
+  console.log(state.form.email);
+  console.log(state.form.password);
+  axios
+    .post("/sign-in", state.form)
+    .then((res) => {
+      console.log(res);
+      alert("로그인 하였습니다.");
+      // 로그인 성공한 경우
+      console.log(res.data);
+      // alert("로그인 하였습니다.");
+      router.push("/");
+    })
+    .catch((error) => {
+      // 로그인 실패한 경우
+      console.log(error);
+      alert("아이디와 비밀번호를 확인해 주세요.");
+    });
+};
+</script>
