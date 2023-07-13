7c0add2759e1cb0caa8deb19d85e6f722c82ed06
jenny <jenny.es2005@gmail.com>
2023-07-13 16:32:09 +0900

    feat: Sign Up Page(회원가입 페이지) 화면 구현

--- frontend/src/views/SignIn.vue
+++ frontend/src/views/SignIn.vue
@@ -1,10 +1,6 @@
 <template>
   <div>
-    <v-img
-      class="mx-auto my-6"
-      max-width="228"
-      src="https://cdn.vuetifyjs.com/docs/images/logos/vuetify-logo-v3-slim-text-light.svg"
-    ></v-img>
+    <v-img class="mx-auto my-6" max-width="50" src="@/img/logo.png"></v-img>
 
     <v-card
       class="mx-auto pa-12 pb-8"
--- frontend/src/views/SignUp.vue
+++ frontend/src/views/SignUp.vue
@@ -1,10 +1,6 @@
 <template>
   <div>
-    <v-img
-      class="mx-auto my-6"
-      max-width="228"
-      src="https://cdn.vuetifyjs.com/docs/images/logos/vuetify-logo-v3-slim-text-light.svg"
-    ></v-img>
+    <v-img class="mx-auto my-6" max-width="50" src="@/img/logo.png"></v-img>
 
     <v-card
       class="mx-auto pa-12 pb-8"
@@ -12,19 +8,23 @@
       max-width="448"
       rounded="lg"
     >
-      <div class="text-subtitle-1 text-medium-emphasis">Account</div>
+      <v-card-text class="text-center text-blue" style="font-size: x-large">
+        Sign up</v-card-text
+      >
 
       <v-text-field
-        clearable
-        label="Label"
-        prepend-icon="$vuetify"
+        v-model="email"
+        density="compact"
+        placeholder="Email address"
+        prepend-inner-icon="mdi-email-outline"
         variant="outlined"
       ></v-text-field>
 
       <v-text-field
-        clearable
-        label="Label"
-        prepend-icon="$vuetify"
+        v-model="name"
+        density="compact"
+        placeholder="Name"
+        prepend-inner-icon="mdi-account-outline"
         variant="outlined"
       ></v-text-field>
 
@@ -38,19 +38,35 @@
         @click:append-inner="visible = !visible"
       ></v-text-field>
 
-      <v-card class="mb-12" color="surface-variant" variant="tonal">
+      <v-text-field
+        :append-inner-icon="visible ? 'mdi-eye-off' : 'mdi-eye'"
+        :type="visible ? 'text' : 'password'"
+        density="compact"
+        placeholder="Confirm password"
+        prepend-inner-icon="mdi-checkbox-marked-circle-outline"
+        variant="outlined"
+        @click:append-inner="visible = !visible"
+      ></v-text-field>
+
+      <!--      <v-card class="mb-12" color="surface-variant" variant="tonal">
         <v-card-text class="text-medium-emphasis text-caption">
           Warning: After 3 consecutive failed login attempts, you account will
           be temporarily locked for three hours. If you must login now, you can
           also click "Forgot login password?" below to reset the login password.
         </v-card-text>
-      </v-card>
+      </v-card>-->
+
+      <v-checkbox
+        v-model="terms"
+        color="secondary"
+        label="I agree to site terms and conditions"
+      ></v-checkbox>
 
       <v-btn block class="mb-8" color="blue" size="large" variant="tonal">
-        Log In
+        Sign Up
       </v-btn>
 
-      <v-card-text class="text-center">
+      <!--      <v-card-text class="text-center">
         <a
           class="text-blue text-decoration-none"
           href="#"
@@ -59,7 +75,7 @@
         >
           Sign up now <v-icon icon="mdi-chevron-right"></v-icon>
         </a>
-      </v-card-text>
+      </v-card-text>-->
     </v-card>
   </div>
 </template>
@@ -67,6 +83,10 @@
 export default {
   data: () => ({
     visible: false,
+    email: null,
+    name: null,
+    password: null,
+    terms: false,
   }),
 };
 </script>
